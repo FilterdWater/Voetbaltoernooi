da66c76ceac253139466e0c95fbe6e05dd716986
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2023-11-27 12:34:58 +0100

    Test commit

--- dist/index.php
+++ dist/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VoetbalToernooi | Home</title>
 </head>
-<body>
-  <p class="bg-400">test</p>
+<body class="bg-black flex">
+    <?php echo '<div> je moder</div>'; ?>
 </body>
 </html>
