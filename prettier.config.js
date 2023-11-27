ac2a6504c7eb537736153633a401d1a425781ce3
SanderFrakking <s.d.frakking@gmail.com>
2023-11-27 11:53:29 +0100

    plugin install

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").config} */
+const config = {
+  tailwindConfig: './tailwind.config.js',
+  plugins: ['@prettier/plugin-php', 'prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 500,
+  bracketSameLine: true,
+  phpVersion: '7.4',
+};
+module.exports = config;
