2eb65c5d137a800261d7b7e99c593cface030dec
SanderFrakking <s.d.frakking@gmail.com>
2023-12-01 23:48:56 +0100

    login/register system
    -bug fixes
    -password hashing

--- dist/php/classes/account.php
+++ dist/php/classes/account.php
@@ -2,18 +2,20 @@
 class account
 {
   public $conn;
-  private $errorArray = [];
+  private $errorArray = ['firstName' => [], 'lastName' => [], 'password' => [], 'password2' => [], 'email' => []];
   private $aangemeld = false;
 
-  public static $voornaamMessage = 'Voornaam moet tussen de 2 en 30 cijfers zijn';
-  public static $achternaamMessage = 'Achternaam moet tussen de 2 en 30 cijfers zijn';
+  public static $voornaamMessage = 'Voornaam moet tussen de 2 en 30 letters zijn';
+  public static $achternaamMessage = 'Achternaam moet tussen de 2 en 30 letters zijn';
   public static $emailKort = 'email moet minimaal 3 cijfers zijn';
   public static $emailNietGeldig = 'email adres is niet geldig';
   public static $emailGebruikt = 'Er bestaat al een account met dit email adres';
   public static $wachtwoordlengte = 'Achternaam moet tussen de 4 en 50 cijfers zijn';
   public static $loginMislukt = 'login mislukt probeer het opnieuw';
   public static $accountBestaatNiet = 'Sorry dit account bestaat niet';
-  public static $wachtwoordincorrect = 'Ongeldig wachtwoord';
+  public static $wachtwoordincorrect = 'Wrong password';
+  public static $passwordDoesntMatch = 'wachtwoord komt niet overheen';
+  public static $fieldEmpty = 'this field must be filled to continue';
 
   //when yu create a new instance of this class please provide an PDO object
   public function __construct($pdo)
@@ -21,54 +23,59 @@ class account
     $this->conn = $pdo;
   }
 
-  public function register($fn, $ln, $pw, $em, $admin)
+  public function register($firstName, $lastName, $password, $password2, $email, $admin)
   {
-    $this->CheckVoornaam($fn);
-    $this->CheckAchternaam($ln);
-    $this->CheckEmail($em);
-    $this->checkPassword($pw);
+    $this->CheckVoornaam($firstName);
+    $this->CheckAchternaam($lastName);
+    $this->CheckEmail($email);
+    $this->checkPassword($password);
+    $this->compPassword($password, $password2);
 
-    if (empty($this->errorArray)) {
+    print_r($this->errorArray);
+
+    if (empty($this->errorArray['firstName']) && empty($this->errorArray['lastName']) && empty($this->errorArray['password']) && empty($this->errorArray['password2']) && empty($this->errorArray['email'])) {
       $this->errorArray = [];
       $this->aangemeld = true;
-      return $this->stopInDB($fn, $ln, $pw, $em, $admin);
+      return $this->stopInDB($firstName, $lastName, $password, $email, $admin);
     }
     return false;
   }
 
-  public function login($em, $ww)
+  public function login($email, $password)
   {
-    $user_data = $this->getUser($em);
+    $user_data = $this->getUser($email);
     if ($user_data) {
-      if ($user_data['wachtwoord'] !== $ww) {
-        $this->errorArray[] = account::$wachtwoordincorrect;
+      if (password_verify($password, $user_data['password'])) {
+        array_push($this->errorArray['password'], account::$wachtwoordincorrect);
         return false;
       }
       return true;
     } else {
-      $this->errorArray[] = account::$accountBestaatNiet;
+      array_push($this->errorArray['password'], account::$accountBestaatNiet);
       return false;
     }
   }
 
-  public function getUser($em)
+  public function getUser($email)
   {
     $query = 'SELECT * FROM user WHERE email = :email';
     $stmt = $this->conn->prepare($query);
-    $stmt->bindParam(':email', $em);
+    $stmt->bindParam(':email', $email);
     $stmt->execute();
     $gebruiker = $stmt->fetch(PDO::FETCH_ASSOC);
     return $gebruiker;
   }
 
-  public function stopInDB($fn, $ln, $pw, $em, $admin)
+  public function stopInDB($firstName, $lastName, $password, $email, $admin)
   {
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
     $query = 'INSERT INTO user (first_name, last_name, password, email, admin) VALUES (?, ?, ?, ?, ?)';
+
     $stmt = $this->conn->prepare($query);
-    $stmt->bindParam(1, $fn, PDO::PARAM_STR);
-    $stmt->bindParam(2, $ln, PDO::PARAM_STR);
-    $stmt->bindParam(3, $pw, PDO::PARAM_STR);
-    $stmt->bindParam(4, $em, PDO::PARAM_STR);
+    $stmt->bindParam(1, $firstName, PDO::PARAM_STR);
+    $stmt->bindParam(2, $lastName, PDO::PARAM_STR);
+    $stmt->bindParam(3, $password_hash, PDO::PARAM_STR);
+    $stmt->bindParam(4, $email, PDO::PARAM_STR);
     $stmt->bindParam(5, $admin, PDO::PARAM_INT);
     return $stmt->execute();
   }
@@ -76,45 +83,57 @@ class account
   public function CheckVoornaam($vn)
   {
     if (strlen($vn) < 2 || strlen($vn) > 25) {
-      array_push($this->errorArray, account::$voornaamMessage);
+      array_push($this->errorArray['firstName'], account::$voornaamMessage);
     }
   }
 
   public function CheckAchternaam($an)
   {
     if (strlen($an) < 2 || strlen($an) > 30) {
-      array_push($this->errorArray, account::$achternaamMessage);
+      array_push($this->errorArray['lastName'], account::$achternaamMessage);
     }
   }
 
-  public function CheckEmail($em)
+  public function CheckEmail($email)
   {
-    if (strlen($em) < 3) {
-      array_push($this->errorArray, account::$emailKort);
+    if (strlen($email) < 3) {
+      array_push($this->errorArray['email'], account::$emailKort);
       return;
     }
-    if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-      array_push($this->errorArray, account::$emailNietGeldig);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      array_push($this->errorArray['email'], account::$emailNietGeldig);
     }
-    $query = "SELECT email FROM `user` WHERE email LIKE '%$em%'";
+    $query = "SELECT email FROM `user` WHERE email LIKE '%$email%'";
     $stmt = $this->conn->query($query);
     if ($stmt->fetch(PDO::FETCH_ASSOC) != 0) {
-      array_push($this->errorArray, account::$emailGebruikt);
+      array_push($this->errorArray['email'], account::$emailGebruikt);
     }
   }
 
-  public function checkPassword($ww)
+  public function checkPassword($password)
   {
-    if (strlen($ww) < 5 || strlen($ww) > 50) {
-      array_push($this->errorArray, account::$wachtwoordlengte);
+    if (strlen($password) < 5 || strlen($password) > 50) {
+      array_push($this->errorArray['password'], account::$wachtwoordlengte);
     }
   }
 
-  public function getError($error)
+  public function compPassword($password, $password2)
   {
-    if (in_array($error, $this->errorArray)) {
-      return "<span class='text-red-600 font-bold text-sm'>$error</span>";
+    if ($password !== $password2) {
+      array_push($this->errorArray['password2'], account::$passwordDoesntMatch);
     }
+  }
+
+  public function getError($type)
+  {
+    //print_r($this->errorArray[$type]);
+    if (isset($this->errorArray[$type])) {
+      $errors[] = $this->errorArray[$type]; //pakt de errors van het type
+      foreach ($errors[0] as $error) {
+        return "<span class='text-red-600 font-bold text-sm'>$error</span>";
+      }
+    }
+    return '';
   }
 }
 ?>
