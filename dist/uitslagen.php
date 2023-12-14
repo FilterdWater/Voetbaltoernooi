c656ba1f5b4963aebdd7e3eda11c4c02eeb12ab3
Lars <129171301+FilterdWater@users.noreply.github.com>
2023-12-14 21:57:27 +0100

    added some small stuff
    uitslagen.php and fixed all header links

---
+++ dist/uitslagen.php
@@ -0,0 +1,16 @@
+<?php
+$pageName = 'Uitslagen';
+require_once 'php/functions.php';
+redirectToLoginIfNotLoggedIn();
+require_once 'php/db.php';
+require_once 'php/components.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <?php htmlhead($pageName); ?>
+<body>
+  <?php htmlheader(); ?>
+</body>
+<script src="js/functions.js"></script>
+<script>document.addEventListener('DOMContentLoaded', hamburger());</script>
+</html>
